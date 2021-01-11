1440f9f8d9c5c33b4f7ebe5d0b045f232d58a19c
simomarsili <simo.marsili@gmail.com>
2021-01-11 13:26:09 +0100

    fix package.py

--- equilibrated_systems/package.py
+++ equilibrated_systems/package.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 """Template package file"""
-__title__ = 'template_project'
+__title__ = 'equilbrated_systems'
 __version__ = '0.1'
 __author__ = 'Simone Marsili'
 __summary__ = ''
 __url__ = 'https://github.com/simomarsili/template_project'
 __email__ = 'simo.marsili@gmail.com'
 __license__ = 'BSD 3-Clause'
-__copyright__ = 'Copyright (c) 2020, Simone Marsili'
+__copyright__ = 'Copyright (c) 2021, Simone Marsili'
 __classifiers__ = [
     'Development Status :: 1 - Planning',
     'License :: OSI Approved :: BSD License',
