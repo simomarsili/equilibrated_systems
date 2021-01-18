251c4d98aa2956d82a587854435058069ce5e460
simomarsili <simo.marsili@gmail.com>
2021-01-18 18:11:33 +0100

    add initialize_sampler

--- equilibrated_systems/lj_fluid/sampler.py
+++ equilibrated_systems/lj_fluid/sampler.py
@@ -4,6 +4,7 @@
 from math import ceil
 from pathlib import Path
 
+from mmdemux import extract_trajectory_to_file
 from mmlite.gromacs import generate_gromacs_input
 from mmlite.multistate import SAMSSampler, propagator
 from mmlite.systems import LennardJonesFluid
@@ -12,50 +13,58 @@ from simtk import unit
 test = LennardJonesFluid(nparticles=100, reduced_density=0.01)  # test system
 temperatures = 298 * unit.kelvin
 pressure = 1.0 * unit.atmosphere
-# pressure = 298 * unit.atmosphere
-production_iterations = 1000  # total number of sampler iterations (state updates)
+# production_iterations = 1000  # total number of sampler iterations (state updates)
+production_iterations = 100  # total number of sampler iterations (state updates)
 burnin_iterations = 100  # number of burnin iterations (#iterations)
 timestep = 1.0 * unit.femtoseconds
 state_update_steps = 1000  # stride in steps between state update (#steps)
 checkpoint_iterations = 10  # checkpoint_interval (#iterations)
-container = Path('frames/trj.nc')  # trajectory filepath
+
+ms_container = Path('frames/trj.nc')  # trajectory filepath
+target_container = Path('frames/trj.0.nc')  # trajectory filepath
+
+
+def initialize_sampler():
+    """Initialize a multistate sampler."""
+
+    # initialize sampler
+    n_iterations = production_iterations + burnin_iterations
+
+    smp = SAMSSampler(number_of_iterations=n_iterations,
+                      mcmc_moves=propagator(timestep=timestep,
+                                            n_steps=state_update_steps),
+                      online_analysis_interval=None)
+
+    # set sampler states and positions
+    smp.from_testsystem(test,
+                        temperatures=temperatures,
+                        pressure=pressure,
+                        stride=checkpoint_iterations,
+                        storage=ms_container)
+    return smp
+
 
 if __name__ == '__main__':
 
-    n_iterations = production_iterations + burnin_iterations
-
     burnin_iterations += 1  # discard starting configuration from trajectory
-
-    # initialize sampler
-    sampler = SAMSSampler(number_of_iterations=n_iterations,
-                          mcmc_moves=propagator(timestep=timestep,
-                                                n_steps=state_update_steps),
-                          online_analysis_interval=None)
 
     start_frame = ceil(burnin_iterations / checkpoint_iterations)
 
-    print('''
-        temperatures: %r,
-        n_iterations: %r,
-        burnin: %r,
-        state_update_interval: %r,
-        checkpoint_interval: %r,
-        start_frame: %r
-        ''' % (temperatures, n_iterations, burnin_iterations,
-               state_update_steps, checkpoint_iterations, start_frame))
-
-    # set sampler states and positions
-    sampler.from_testsystem(test,
-                            temperatures=temperatures,
-                            pressure=pressure,
-                            stride=checkpoint_iterations)
+    # initialize sampler
+    sampler = initialize_sampler()
 
     # run the simulation
     sampler.run()
 
     # save the sampled configurations at the target thermodynamic state
-    sampler.demux(0, to_file=container, start_frame=start_frame)
+    extract_trajectory_to_file(nc_path=ms_container,
+                               out_path=target_container,
+                               state_index=0,
+                               start_frame=start_frame)
 
     # start_frame = 10
     # create gromacs .top and .gro files in the target directory
-    generate_gromacs_input(container, sampler)
+    generate_gromacs_input(target_container,
+                           topology=test.topology,
+                           system=test.system,
+                           split='.gro')
