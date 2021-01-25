3b80f37c5e3316c8ebf8232e7c69d0616fb37ab8
simomarsili <simo.marsili@gmail.com>
2021-01-25 16:12:12 +0100

    split prepare.py from sampler.py

--- equilibrated_systems/lj_fluid/sampler.py
+++ equilibrated_systems/lj_fluid/sampler.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 """Functions for sampling."""
 # pylint: disable=protected-access, no-member
-from math import ceil
+import logging
 from pathlib import Path
 
-from mmdemux import extract_trajectory_to_file
-from mmlite.gromacs import generate_gromacs_input
 from mmlite.multistate import SAMSSampler, propagator
 from mmlite.systems import LennardJonesFluid
 from simtk import unit
@@ -13,7 +11,7 @@ from simtk import unit
 test = LennardJonesFluid(nparticles=100, reduced_density=0.01)  # test system
 temperatures = 298 * unit.kelvin
 pressure = 1.0 * unit.atmosphere
-production_iterations = 1000  # total number of sampler iterations (state updates)
+production_iterations = 100  # total number of sampler iterations (state updates)
 burnin_iterations = 100  # number of burnin iterations (#iterations)
 timestep = 1.0 * unit.femtoseconds
 state_update_steps = 1000  # stride in steps between state update (#steps)
@@ -21,6 +19,11 @@ checkpoint_iterations = 10  # checkpoint_interval (#iterations)
 
 ms_container = Path('frames/trj.nc')  # trajectory filepath
 target_container = Path('frames/trj.0.nc')  # trajectory filepath
+
+verbose_module = 'openmmtools.multistate'
+
+if verbose_module:
+    logging.getLogger(verbose_module).setLevel(logging.DEBUG)
 
 
 def initialize_sampler():
@@ -45,25 +48,8 @@ def initialize_sampler():
 
 if __name__ == '__main__':
 
-    burnin_iterations += 1  # discard starting configuration from trajectory
-
-    start_frame = ceil(burnin_iterations / checkpoint_iterations)
-
     # initialize sampler
     sampler = initialize_sampler()
 
     # run the simulation
     sampler.run()
-
-    # save the sampled configurations at the target thermodynamic state
-    extract_trajectory_to_file(nc_path=ms_container,
-                               out_path=target_container,
-                               state_index=0,
-                               start_frame=start_frame)
-
-    # start_frame = 10
-    # create gromacs .top and .gro files in the target directory
-    generate_gromacs_input(target_container,
-                           topology=test.topology,
-                           system=test.system,
-                           split='.gro')
