4f929cdbebb9bbb36bc94e3eba030291ca6019df
simomarsili <simo.marsili@gmail.com>
2021-02-22 17:03:10 +0100

    set metadata

--- equilibrated_systems/host_guest_implicit_torsional/app.py
+++ equilibrated_systems/host_guest_implicit_torsional/app.py
@@ -14,21 +14,22 @@ test = systems.HostGuestImplicit()  # test system
 temperature = 298 * unit.kelvin
 pressure = None
 
-# sampler_class = SAMSSampler
 sampler_class = ReplicaExchangeSampler
-timestep = 1.0 * unit.femtoseconds
+timestep = 2.0 * unit.femtoseconds
 state_update_steps = 1000  # stride in steps between state update (#steps)
-checkpoint_iterations = 1  # checkpoint_interval (#iterations)
+checkpoint_iterations = 10  # checkpoint_interval (#iterations)
 
 protocol = dict(  # define the scaling protocol as a dict
     lambda_torsions=list(np.logspace(0, -1, 4)))
 ref_state_index = 0
+
 reference_thermodynamic_state = mmtools.states.ThermodynamicState(
     system=test.system, temperature=temperature, pressure=pressure)
 thermodynamic_states = create_compound_states(reference_thermodynamic_state,
                                               test.topology,
                                               protocol,
                                               region='default')
+metadata = create_compound_states.metadata
 
 ms_container = Path('frames/trj.nc')  # trajectory filepath
 verbose_module = 'openmmtools.multistate'
