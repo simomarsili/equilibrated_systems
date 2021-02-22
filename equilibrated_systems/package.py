4107087f9c7594b72f84f28761393969f69834d4
simomarsili <simo.marsili@gmail.com>
2021-02-22 17:09:14 +0100

    bump version to v0.2.15

--- equilibrated_systems/package.py
+++ equilibrated_systems/package.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Template package file"""
 __title__ = 'equilbrated_systems'
-__version__ = '0.2.14'
+__version__ = '0.2.15'
 __author__ = 'Simone Marsili'
 __summary__ = ''
 __url__ = 'https://github.com/simomarsili/template_project'
